477ad90a7e1117a6ebe44a4450c89cf93d3b97e5
edwardcashmere <eomondi06@gmail.com>
2020-06-23 07:26:01 +0300

    fixed a bug

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/contact',require('./routes/contact'));
 
 
 //serve static  assets in production
-if(NODE_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
     //set static assets
     app.use(express.static('client/build'));
     app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'client','build','index.html')))
